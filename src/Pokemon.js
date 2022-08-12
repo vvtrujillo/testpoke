155fd80482c70a688c68029a1934881044bd5ffc
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-12 12:26:18 -0400

    aassss

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -7,7 +7,7 @@ const Pokemon = () => {
     const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
     const estado = UseFetch(url);
     const {cargando, data} = estado;
-    cargando? console.log('cargando') : console.lo4(data.results);
+    cargando? console.log('cargando') : console.log(data.results);
     
     return(
         <div>
